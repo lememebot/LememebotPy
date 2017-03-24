73c54001e036099a76c479e0ab175b6422d7776d
Dean <deankevor@gmail.com>
2017-03-24 20:35:00 +0300

    Committing comment containing TOKEN IDs for all the devs' bots.

--- LememebotPy/LememebotPy/Program.py
+++ LememebotPy/LememebotPy/Program.py
@@ -10,10 +10,16 @@ async def on_ready():
 async def on_message(message):
     print(message)
 
-client.run('E78l1UAmbc47wUEpqn0V6Km8fmGploz5')
+client.run('Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0')
 
 # URL to add:
 #   (Malul):  https://discordapp.com/api/oauth2/authorize?client_id=293837616787488780&scope=bot&permissions=0
-#   (Zafig):  https://discordapp.com/api/oauth2/authorize?client_id=&scope=bot&permissions=0
-#   (Fknfgt): https://discordapp.com/api/oauth2/authorize?client_id=&scope=bot&permissions=0
-#   (John):   https://discordapp.com/api/oauth2/authorize?client_id=&scope=bot&permissions=0
+#   (Zafig):  https://discordapp.com/api/oauth2/authorize?client_id=*DUMMY*&scope=bot&permissions=0
+#   (Fknfgt): https://discordapp.com/api/oauth2/authorize?client_id=294883627165024258&scope=bot&permissions=0
+#   (John):   https://discordapp.com/api/oauth2/authorize?client_id=*DUMMY*&scope=bot&permissions=0
+
+# TOKEN IDS:
+#   (Malul):  https://discordapp.com/api/oauth2/authorize?client_id=293837616787488780&scope=bot&permissions=0
+#   (Zafig):  TODO
+#   (Fknfgt): Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0
+#   (John):   TODO
