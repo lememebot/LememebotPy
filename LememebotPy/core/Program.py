89c6f326340aacb58d2f45b26130a455bcba2cf0
Guy Melul <guymalul95@gmail.com>
2017-03-24 20:53:55 +0300

    token
    tokem

--- LememebotPy/core/Program.py
+++ LememebotPy/core/Program.py
@@ -1,4 +1,5 @@
 import discord,asyncio
+import Token
 
 client = discord.Client()
 
@@ -10,16 +11,16 @@ async def on_ready():
 async def on_message(message):
     print(message)
 
-client.run('Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0')
+client.run(discord_token)
 
 # URL to add:
-#   (Malul):  https://discordapp.com/api/oauth2/authorize?client_id=293837616787488780&scope=bot&permissions=0
+#   (Malul):  https://discordapp.com/api/oauth2/authorize?client_id=294886929735090186&scope=bot&permissions=0
 #   (Zafig):  https://discordapp.com/api/oauth2/authorize?client_id=*DUMMY*&scope=bot&permissions=0
 #   (Fknfgt): https://discordapp.com/api/oauth2/authorize?client_id=294883627165024258&scope=bot&permissions=0
 #   (John):   https://discordapp.com/api/oauth2/authorize?client_id=*DUMMY*&scope=bot&permissions=0
 
-# TOKEN IDS:
-#   (Malul):  https://discordapp.com/api/oauth2/authorize?client_id=293837616787488780&scope=bot&permissions=0
+# Tokens:
+#   (Malul):  rUVg8F4locEucRGuTSKkPX2R6qX4ycZO
 #   (Zafig):  MjkzMDk1NzY0MDEwMzM2MjY2.C7bqXA.QjOGFq-bP409lsLH6T7TNzS1LgQgit
 #   (Fknfgt): Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0
 #   (John):   TODO
---
+++ LememebotPy/core/Token.py
@@ -0,0 +1,6 @@
+# Tokens:
+#   (Malul):  rUVg8F4locEucRGuTSKkPX2R6qX4ycZO
+#   (Zafig):  MjkzMDk1NzY0MDEwMzM2MjY2.C7bqXA.QjOGFq-bP409lsLH6T7TNzS1LgQgit
+#   (Fknfgt): Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0
+#   (John):   TODO
+discord_token='rUVg8F4locEucRGuTSKkPX2R6qX4ycZO'
