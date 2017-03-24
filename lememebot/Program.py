2a1c2d83eae737a6718451ea808392e1a2f0c594
Dean <deankevor@gmail.com>
2017-03-25 02:44:59 +0300

    - Added files that were relevant/missing to the last commit.

---
+++ lememebot/Program.py
@@ -0,0 +1,43 @@
+import discord
+import asyncio
+
+from Token import get_discord_token
+from handlers.Cleverbot import on_message as clv_handle
+from handlers.HoferHandler import on_message as hofer_handle
+from handlers.Overwatch import on_message as overwatch_handle
+from handlers.RemindMe import on_message as remindme_handle
+
+handlers = [clv_handle, hofer_handle, overwatch_handle, remindme_handle];
+
+print('')
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as ',client.user.name,'(',client.user.id,')')
+
+@client.event
+async def on_message(message):
+    if (message.author.id != client.user.id):
+        if('!!quit' == message.content):
+            await client.logout()
+        else:
+            print('[DEBUG] ',message.author,': ', message.content)
+
+            for handle in handlers:
+                print("Malullll")
+                handle(message)
+
+client.run(get_discord_token())
+
+# URL to add:
+#   (Malul):  https://discordapp.com/api/oauth2/authorize?client_id=294886929735090186&scope=bot&permissions=0
+#   (Zafig):  https://discordapp.com/api/oauth2/authorize?client_id=*DUMMY*&scope=bot&permissions=0
+#   (Fknfgt): https://discordapp.com/api/oauth2/authorize?client_id=294883627165024258&scope=bot&permissions=0
+#   (John):   https://discordapp.com/api/oauth2/authorize?client_id=*DUMMY*&scope=bot&permissions=0
+
+# Tokens:
+#   (Malul):  rUVg8F4locEucRGuTSKkPX2R6qX4ycZO
+#   (Zafig):  MjkzMDk1NzY0MDEwMzM2MjY2.C7bqXA.QjOGFq-bP409lsLH6T7TNzS1LgQgit
+#   (Fknfgt): Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0
+#   (John):   TODO
---
+++ lememebot/Token.py
@@ -0,0 +1,7 @@
+# Tokens:
+#   (Malul):  Mjk0ODg2OTI5NzM1MDkwMTg2.C7b7Fg.adre6bVKOBEYvJL0QZyUPz5VV2I
+#   (Zafig):  MjkzMDk1NzY0MDEwMzM2MjY2.C7bqXA.QjOGFq-bP409lsLH6T7TNzS1LgQgit
+#   (Fknfgt): Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0
+#   (John):   TODO
+def get_discord_token():
+    return 'Mjk0ODgzNjI3MTY1MDI0MjU4.C7bnWA.T466dvvu3Z0cssV7tuBgfffGRb0'
