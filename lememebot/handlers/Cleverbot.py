2bc0e01a837200d77212918e6ecb6ba08e2e4c9c
Dean <deankevor@gmail.com>
2017-03-25 03:34:32 +0300

    Committing merged conflict fixes & beggining of Cleverbot

--- lememebot/handlers/Cleverbot.py
+++ lememebot/handlers/Cleverbot.py
@@ -1,5 +1,18 @@
+<<<<<<< Updated upstream
 
+=======
+from cleverwrap import CleverWrap
+>>>>>>> Stashed changes
 
+CLEVERBOT_COMMAND = "!cleverbot ";
 # will tell dean to shut the fuck up when he goes nuts
-def on_message(client,message):
-    print('Hey')
+<<<<<<< Updated upstream
+def on_message(client, message):
+    if (message.content.startswith(CLEVERBOT_COMMAND)):
+        cw = CleverWrap("CC1cx7eaidCi0tvh7RfyC7eoYkA")
+        clvrbot_msg_cnt = message.content.split(CLEVERBOT_COMMAND)[1]
+        print ("Calling Cleverbot with " + clvrbot_msg_cnt + "...")
+        answer = cw.say(clvrbot_msg_cnt)
+        client.send_messagge(message.channel, answer)
+        #"Hello Human."
+        cw.reset() # resets the conversation ID and conversation state.
--- lememebot/handlers/Overwatch.py
+++ lememebot/handlers/Overwatch.py
@@ -1,6 +1,9 @@
 
+<<<<<<< Updated upstream
+=======
+#commands = [('hello',on_cmd_hello)]
+>>>>>>> Stashed changes
 
 # will tell dean to shut the fuck up when he goes nuts
 def on_message(client,message):
     print('Hey')
-
