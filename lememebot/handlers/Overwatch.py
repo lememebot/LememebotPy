031f4c8e6d2fdf8c2917dcbe76daebc836ab40c1
Guy Melul <guymalul95@gmail.com>
2017-03-27 22:12:29 +0300

    dean this shit is fo you

--- lememebot/handlers/Overwatch.py
+++ lememebot/handlers/Overwatch.py
@@ -1,5 +1,11 @@
-#commands = [('hello',on_cmd_hello)]
-
 # will tell dean to shut the fuck up when he goes nuts
-def on_message(client,message):
-    print('Hey')
+def on_cmd_hello(client,message,param):
+    print(param)
+    client.send_message(destination=message.channel,content='Playing ' + param)
+
+def get_command_delegate(cmd):
+    return cmd_delegates.get(cmd)
+
+cmd_delegates = { 'hello' : on_cmd_hello }
+
+
